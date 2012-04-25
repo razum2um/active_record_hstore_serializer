8e5ae63345cae513ac68bb98cfc9e5f17fe434db
Vlad Bokov <razum2um@mail.ru>
2012-04-26 02:09:47 +0700

    some nil by default

--- lib/active_record_hstore_serializer/hstore_serializer.rb
+++ lib/active_record_hstore_serializer/hstore_serializer.rb
@@ -1,11 +1,12 @@
 class HstoreSerializer
 
   def self.load(text)
-    return unless text
+    return {} unless text
     text.from_hstore
   end
 
   def self.dump(text)
+    return nil if text.nil?
     text.to_hstore
   end
 end
